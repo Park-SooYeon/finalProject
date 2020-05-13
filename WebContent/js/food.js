237cb0f6a6160152ba75c74014f2ca1bfce7800a
choi-hakgeun <nojak@naver.com>
2020-05-13 17:21:26 +0900

    20200513{fix}WebContent/js/food.js //디테일뷰 이동 경로추가

--- WebContent/js/food.js
+++ WebContent/js/food.js
@@ -1,7 +1,14 @@
+
+//받은 별점 표시
 var rating = $('.rating');
 	
 rating.each(function(){
 	var targetScore = $(this).attr('data-rate');
 	$(this).find('i:nth-child(-n' + targetScore +')').css({color:'#ffc107'});
 });
-		
+
+
+//상세보기 이동
+let detailMove = function() {
+	location.href = './?inc=commonPage/detailView.jsp';
+}
