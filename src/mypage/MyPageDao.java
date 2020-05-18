7fdb981f29259cb5ec509f270a740171da149566
JuHyeon-An <ajh2042@naver.com>
2020-05-18 17:31:54 +0900

    20200518 [feat] trip insert 진행중

--- src/mypage/MyPageDao.java
+++ src/mypage/MyPageDao.java
@@ -1,12 +1,9 @@
 package mypage;
 
-import java.io.File;
-import java.util.List;
-
-import org.apache.commons.fileupload.FileUpload;
 import org.apache.ibatis.session.SqlSession;
 
 import bean.Factory;
+import mypage_mybatis.TripListVo;
 
 public class MyPageDao {
 // 서블릿과 BoardMybatis를 연결해주는 역할
@@ -17,6 +14,28 @@ public class MyPageDao {
 	public MyPageDao() {
 		sqlSession = Factory.getFactory().openSession();
 		//mybatis와 연결되어지는 연결고리
+	}
+	
+	public String insert(TripListVo vo) {
+		String msg = "정상적으로 저장되었습니다.";
+		try {
+			System.out.println("try");
+			int cnt = sqlSession.insert("mypage.insert_trip", vo);
+			System.out.println("본문 저장 : "+cnt);
+			if(cnt<1) {
+				throw new Exception("본문 저장 중 오류발생");
+			}
+				
+			sqlSession.commit();
+		}catch(Exception ex){
+			msg = ex.getMessage();
+			ex.printStackTrace();
+			
+			sqlSession.rollback();
+		}finally {
+			//sqlSession.close();
+			return msg;
+		}
 	}
 	
 }
---
+++ src/mypage_mybatis/TripListVo.java
@@ -0,0 +1,65 @@
+package mypage_mybatis;
+
+public class TripListVo {
+
+	int trip_list_serial;
+	String member_id;
+	String trip_name;
+	int days_count;
+	String start_date;
+	String end_date;
+	int trip_auth;
+	String trip_date;
+	
+	
+	public int getTrip_list_serial() {
+		return trip_list_serial;
+	}
+	public void setTrip_list_serial(int trip_list_serial) {
+		this.trip_list_serial = trip_list_serial;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getTrip_name() {
+		return trip_name;
+	}
+	public void setTrip_name(String trip_name) {
+		this.trip_name = trip_name;
+	}
+	public int getDays_count() {
+		return days_count;
+	}
+	public void setDays_count(int days_count) {
+		this.days_count = days_count;
+	}
+	public String getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(String start_date) {
+		this.start_date = start_date;
+	}
+	public String getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(String end_date) {
+		this.end_date = end_date;
+	}
+	public int getTrip_auth() {
+		return trip_auth;
+	}
+	public void setTrip_auth(int trip_auth) {
+		this.trip_auth = trip_auth;
+	}
+	public String getTrip_date() {
+		return trip_date;
+	}
+	public void setTrip_date(String trip_date) {
+		this.trip_date = trip_date;
+	}
+	
+	
+}
