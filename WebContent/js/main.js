2113731524f0874573806400ff468446ef932098
수연 <cjg03194@gmail.com>
2020-05-18 11:36:17 +0900

    20200518 {fix} main.js conflict 수정

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -25,7 +25,7 @@ main.like = function (ele) {
 };
 
 /* 여행 선택 모달창에서와 관련된 함수들 */
-//모달창에서 추가 버튼 클릭시 작동
+// 모달창에서 추가 버튼 클릭시 작동
 main.makeHeart = function () {
   // 추가버튼 클릭시 좋아요가 선택됨
   nowEle.classList.remove("fa-heart-o");
@@ -67,8 +67,10 @@ var swiper = new Swiper(".swiper-container", {
   slidesPerView: sliderPage,
   paginationClickable: true,
   spaceBetween: 5,
-  nextButton: ".swiper-button-next",
-  prevButton: ".swiper-button-prev",
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
 });
 
 // 스크린 크기가 변할때마다 slider 개수를 다르게 표기
@@ -86,22 +88,5 @@ mql.addListener(function (e) {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       }
-});
-
-// 스크린 크기가 변할때마다 slider 개수를 다르게 표기
-mql.addListener(function(e) {
-    if(e.matches) {
-    	sliderPage = 3;
-    } else {
-    	sliderPage = 4;
-    }
-    var swiper = new Swiper('.swiper-container', {
-        slidesPerView: sliderPage,
-        paginationClickable: true,
-        spaceBetween: 5,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          },
-    });
+  })
 });
