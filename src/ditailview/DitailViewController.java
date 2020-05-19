0d7791de4d628ae5a372cb10109285c7e370ffdc
choi-hakgeun <nojak@naver.com>
2020-05-19 17:45:40 +0900

    20200519{close}상세보기 컨트롤러 작성

---
+++ src/ditailview/DitailViewController.java
@@ -0,0 +1,31 @@
+package ditailview;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import bean.DitailViewVo;
+
+@Controller
+public class DitailViewController {
+	DitailViewDao dao;
+	
+	public DitailViewController(DitailViewDao dao) {
+		this.dao = dao;
+	}
+	
+	@RequestMapping(value = "/view.dv", method= {RequestMethod.GET})
+	public ModelAndView view(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		DitailViewVo vo = null;
+		int place_serial = Integer.parseInt(req.getParameter("code"));
+		
+		
+		
+		return mv;
+	}
+
+}
