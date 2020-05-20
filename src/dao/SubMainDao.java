235096317095d7324d2f915ab80411707774ea68
수연 <cjg03194@gmail.com>
2020-05-20 10:48:29 +0900

    수정중

--- src/dao/SubMainDao.java
+++ src/dao/SubMainDao.java
@@ -1,5 +1,15 @@
 package dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import bean.Factory;
+
 public class SubMainDao { 
+	
+	SqlSession sqlSession;
+	
+	public SubMainDao() {
+		sqlSession = Factory.getFactory().openSession();
+	}
 	
 }
--- src/vo/MainVo.java
+++ src/vo/MainVo.java
@@ -1,5 +1,20 @@
 package vo;
 
 public class MainVo {
-
+	int local;
+	int menu;
+	
+	
+	public int getLocal() {
+		return local;
+	}
+	public void setLocal(int local) {
+		this.local = local;
+	}
+	public int getMenu() {
+		return menu;
+	}
+	public void setMenu(int menu) {
+		this.menu = menu;
+	}
 }
