8f6825c898d1bc5034b5abc300d1057d97fead17
Jieun Lee <lje7128@gmail.com>
2020-05-21 09:34:57 +0900

    20200521 [feat] admin - hotel 목록 조회 작업중

--- src/partner/PtnHtCompDao.java
+++ src/partner/PtnHtCompDao.java
@@ -1,7 +1,10 @@
 package partner;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
+import bean.DitailViewVo;
 import bean.Factory;
 
 public class PtnHtCompDao {
@@ -10,4 +13,16 @@ public class PtnHtCompDao {
 	public PtnHtCompDao() {
 		sqlSession = Factory.getFactory().openSession();
 	}
-}
+	
+	public List<DitailViewVo> select(){
+		List<DitailViewVo> list = null;
+		
+		try {
+			list = sqlSession.selectList("hotel.select");
+		}catch(Exception ex) {
+			ex.toString();
+		}finally {
+			return list;
+		}
+	}
+} 
