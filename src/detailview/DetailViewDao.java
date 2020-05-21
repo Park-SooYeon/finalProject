a0a453025d91d6da558cefdd7fadb7927ce04250
choi-hakgeun <nojak@naver.com>
2020-05-21 18:05:04 +0900

    20200521{close}sysout 삭제, PlaceVo 임포트

--- src/detailview/DetailViewDao.java
+++ src/detailview/DetailViewDao.java
@@ -4,19 +4,20 @@ import org.apache.ibatis.session.SqlSession;
 
 import bean.DetailViewVo;
 import bean.Factory;
+import bean.PlaceVo;
 
 public class DetailViewDao {
 	SqlSession sqlSession;
 	
 	public DetailViewDao() {
-		sqlSession = Factory.getFactory().openSession();
+		sqlSession = Factory.getFactory().openSession();		
 	}
 	
-	public DetailViewVo view(int place_serial) {
+	public DetailViewVo view(int place_serial) {		
 		DetailViewVo vo = null;
+		PlaceVo pVo = null;
 		try {
-			vo = sqlSession.selectOne("detailView.view", place_serial);
-			System.out.println(vo);
+			vo = sqlSession.selectOne("detailView.view", place_serial);			
 			
 		}catch(Exception ex) {
 			ex.printStackTrace();
