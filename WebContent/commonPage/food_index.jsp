309e883b5b67bf098c38a3e1c5ae75262cc7be5a
수연 <cjg03194@gmail.com>
2020-05-27 12:17:23 +0900

    20200527 {feat} 관광 타입별 관광지 조회 기능 추가 및 filter.js 코드 리팩토링

--- WebContent/commonPage/food_index.jsp
+++ WebContent/commonPage/food_index.jsp
@@ -22,40 +22,7 @@
 <script src="js/food_map.js"></script>
 
 <script>
-let local = ${param.local} + "";
-if(${!(empty param.local)}) {
-	let chk_obj = document.getElementsByName("local_type");
-	//let chk_size = chk_obj.length;
-	
-	// 지역 체크 하기
-	for(i of chk_obj) {
-		if(local == i.value) {
-			i.checked = true;
-			filter.local.push(i.value);
-			let val = $("label[for='" + i.id + "']").text();
-			// 필터 요소 추가
-			$('#filter').append('<button onclick="filter.remove(this)" id="' + i.id + '" class="genric-btn info-border radius">'+ val +'<i class="fa fa-times pl-2"></i></button>');
-			// filter.check(i);
-			break;
-		}
-	}
-}
-
-switch(${param.menu}) {
-case 12:
-	$('#check_happy').click();
-	break;
-case 14:
-	$('#check_culture').click();
-	break;
-case 39:
-	$('#check_food').click();
-	break;
-case 15:
-	$('#check_festival').click();
-	break;
-}
-
+filter.init(${param.menu}, ${param.local});
 </script>
 </body>
 </html>
--- WebContent/js/filter.js
+++ WebContent/js/filter.js
@@ -11,133 +11,137 @@ $('#widthsort').on("click", function() {
 
 // 관광지, 음식점, 축제 중 한가지 filter 조건만 보이기
 $('#check_happy').on("click", function() {
-	filter.menu = 12;
+	menuSelect(12);
+	
+	// 다른 필터 조건 숨기기
 	$('#happy').css("display", '');
 	$('#food').css("display", 'none');
 	$('#festival').css("display", 'none');
 });
 $('#check_culture').on("click", function() {
-	filter.menu = 14;
+	menuSelect(14);
+	
+	// 다른 필터 조건 숨기기
 	$('#happy').css("display", 'none');
 	$('#food').css("display", 'none');
 	$('#festival').css("display", 'none');
 });
 $('#check_food').on("click", function() {
-	filter.menu = 39;
+	menuSelect(39);
+	
+	// 다른 필터 조건 숨기기
 	$('#food').css("display", '');
 	$('#happy').css("display", 'none');
 	$('#festival').css("display", 'none');
 });
 $('#check_festival').on("click", function() {
-	filter.menu = 15;
+	menuSelect(15);
+	
+	// 다른 필터 조건 숨기기
 	$('#festival').css("display", '');
 	$('#happy').css("display", 'none');
 	$('#food').css("display", 'none');
 });
 
+// 관광 타입 radio 버튼 클릭시 작동하는 함수
+menuSelect = function(menu) {
+	// 관광 타입 설정 및 filter 검색 조건 초기화
+	// local 검색 조건은 유지
+	filter.menu = menu;
+	filter.filter = [];
+	
+	// filter 검색 조건 제거
+	let chk_arr = document.getElementsByName("filter_type");
+	for(let i of chk_arr) {
+		if(i.checked == true) {
+			// 체크 해제
+			i.checked = false;
+			
+			// view의 필터 요소 제거
+			let eleId = i.id;
+			let btnId = $("button[id='" + eleId + "']"); // id가 같은 필터 요소 가져오기
+			btnId.remove();
+		}
+	}
+	
+	// ajax로 데이터 가져오기
+	filter.ajax();
+	
+}
 
-
-// check 박스 클릭시, 필터 요소 추가
+// filter 조건에 따라 작동하는 method를 저장하는 모듈
 let filter = {}
 
-filter.local = [] // 지역 코드를 담을 변수
 filter.menu // 관광 타입을 담을 변수
+filter.local = [] // 지역 코드를 담을 변수
 filter.filter = [] // filter 요소를 담을 변수
 filter.allItems = [] // 모든 검색된 요소들을 담을 변수
+
+
+filter.init = function(menu, local) {
+	alert(local);
+	if(local != undefined) {
+		let chk_obj = document.getElementsByName("local_type");
+		
+		// 지역 검색 조건 추가
+		for(i of chk_obj) {
+			if(local == i.value) {
+				i.checked = true;
+				filter.local.push(i.value);
+				let val = $("label[for='" + i.id + "']").text();
+				
+				// 필터 요소 추가
+				$('#filter').append('<button onclick="filter.remove(this)" id="' + i.id + '" class="genric-btn info-border radius">'+ val +'<i class="fa fa-times pl-2"></i></button>');
+				break;
+			}
+		}
+	}
+
+	// 관광 타입별 선택
+	switch(menu) {
+	case 12:
+		$('#check_happy').click();
+		break;
+	case 14:
+		$('#check_culture').click();
+		break;
+	case 39:
+		$('#check_food').click();
+		break;
+	case 15:
+		$('#check_festival').click();
+		break;
+	}
+}
 
 filter.check = function(ele) {
 	let eleId = ele.getAttribute("id"); // 요소의 id 값
 	let eleName = ele.getAttribute("name"); // 요소의 name 값
+	let check_val = ele.value; // 요소의 value 값
 	let val = $("label[for='" + eleId + "']").text(); // for로 연결된 라벨의 text 값을 가져옴
-	let check_val = ele.value;
-	alert("?? : " + check_val);
-	alert(eleName);
 	
-	if(ele.checked == true) {
-		switch(eleName) {
-		case "filter_type":
-			filter.filter.push(check_val);
-			break;
-		case "local_type":
-			filter.local.push(check_val);
-			break;
-		}
-		
-		// 필터 요소 추가
+	// 체크박스 체크 여부에 따라 view 필터 요소 추가 및 제거
+	// 체크박스 체크 여부에 따라 검색요소 추가 및 제거
+	if(ele.checked == true) { // 체크박스가 체크 되어있으면
+		// view에 필터 요소 추가
 		$('#filter').append('<button onclick="filter.remove(this)" id="' + eleId + '" class="genric-btn info-border radius">'+val+'<i class="fa fa-times pl-2"></i></button>');
-		console.log(filter.menu);
-		console.log(filter.filter);
-		console.log(filter.local);
-		$.ajax({
-			url : "searchList.sb",
-			method : "post",
-			data : {
-				"menu" : filter.menu + "",
-				"local" : filter.local,
-				"filter" : filter.filter
-			},
-			//traditional : true, // 배열을 data로 전달하기 위하여 사용
-			dataType : "json",
-			success : function(data) {
-				alert("성공!");
-				//let items = (data['response'])['body']['items']['item'];
-				items = data;
-				filter.allItems = data;
-				//filter.allItems = filter.allItems.concat(items); // 모든 검색 조건 더하기
-				// 검색 조건 contentID별 정렬
-				filter.allItems.sort(function(a,b) {
-					return a.contentid - b.contentid;
-				})
-				console.log(filter.allItems);
-				
-				filter.makeElement();
-			},
-			error : function(err) {
-				alert("실패!");
-			}
-		});
+
+		// local 타입인지 filter 타입인지 구분하여 각 검색 요소에 추가
+		filter.searchAdd(eleName, check_val);
+		
 	} else {
-		// 검색된 조건에서 제거된 필터에 의해 검색된 요소만 제거
-		let size = filter.allItems.length;
-/*		let removeIndex = []; // 지워야할 contentid를 저장
-		for(let i = 0 ; i < size ; i++) {
-			let item = filter.allItems[i];
-			console.log(filter.allItems[i]['cat1']);
-			if(item['cat1'] === check_val || item['cat2'] === check_val || item['cat3'] === check_val) {
-				removeIndex.push(item['contentid']);
-			}		
-		}*/
-		
-		// 제거된 필터에 포함되어 있는 검색 정보들 제거
-		for(let i = 0 ; i < size ; i++) {
-			let index = filter.allItems.findIndex(a => a.areacode == check_val || a.cat1 === check_val || a.cat2 === check_val || a.cat3 === check_val);
-			console.log(index);
-			if(index !== -1) { // 일치하는 요소가 있을 때에만 배열에서 제거
-				filter.allItems.splice(index, 1);				
-			}
-		}
-		
-		// local, filter 배열에서 제거
-		switch(eleName) {
-		case "filter_type":
-			let idx = filter.filter.findIndex(a => a === check_val)
-			if(idx !== -1) filter.filter.splice(idx, 1);
-			console.log(filter.filter);
-			console.log("idx : " + idx);
-			break;
-		case "local_type":
-			let idy = filter.local.findIndex(a => a == check_val)
-			if(idy !== -1) filter.local.splice(idx, 1);
-			break;
-		}
-		
-		console.log(filter.allItems);
-		// 필터 요소 제거
+		// view에 필터 요소 제거
 		let btnId = $("button[id='" + eleId + "']"); // id가 같은 필터 요소 가져오기
 		btnId.remove();
-		// 검색된 요소가 제거된 items만 다시 그리기
-		filter.makeElement();
+		
+		// local, filter 배열에서 체크 해제된 검색 요소 제거
+		filter.searchRemove(eleName, check_val);
+		
 	}
+	
+	// ajax로 데이터 가져오기
+	filter.ajax();
+	
 }
 
 filter.remove = function(ele) {
@@ -148,41 +152,16 @@ filter.remove = function(ele) {
 	let eleCheck = $("input:checkbox[id='" + eleId + "']");
 	eleCheck.prop("checked", false);
 	
-	
-	// 검색된 조건에서 제거된 필터에 의해 검색된 요소만 제거
+	// 체크가 해제된 체크박스의 name과 value 가져오기
 	let check_val = eleCheck.val();
-	alert(check_val);
 	let eleName = eleCheck.attr("name");
-	alert(eleName);
 	
-	let size = filter.allItems.length;
+	// local, filter 배열에서 체크 해제된 검색 요소 제거
+	filter.searchRemove(eleName, check_val);
 	
-	// 제거된 필터에 포함되어 있는 검색 정보들 제거
-	for(let i = 0 ; i < size ; i++) {
-		let index = filter.allItems.findIndex(a => a.areacode == check_val || a.cat1 === check_val || a.cat2 === check_val || a.cat3 === check_val);
-		console.log(index);
-		if(index !== -1) { // 일치하는 요소가 있을 때에만 배열에서 제거 index == -1 일때도 제거하면 제일 마지막 요소가 제거됨
-			filter.allItems.splice(index, 1);				
-		}
-	}
-	console.log(filter.allItems);
+	// ajax로 데이터 가져오기
+	filter.ajax();
 	
-	// local, filter 배열에서 제거
-	switch(eleName) {
-	case "filter_type":
-		let idx = filter.filter.findIndex(a => a === check_val)
-		if(idx !== -1) filter.filter.splice(idx, 1);
-		console.log(filter.filter);
-		console.log("idx : " + idx);
-		break;
-	case "local_type":
-		let idy = filter.local.findIndex(a => a == check_val)
-		if(idy !== -1) filter.local.splice(idx, 1);
-		break;
-	}
-	
-	// 검색된 요소가 제거된 items만 다시 그리기
-	filter.makeElement();
 }
 
 filter.removeAll = function() {
@@ -196,23 +175,81 @@ filter.removeAll = function() {
 		i.checked = false;
 	}
 	
-	// 모든 필터 요소 지우기
+	// view의 모든 필터 요소 지우기
 	$('#filter').empty();
 	
-	// local, filter 배열 제거
+	// local, filter 배열 비우기
 	filter.local = [];
 	filter.filter = [];
 	
-	// 검색된 요소 전체 그리기
-	filter.allItems = [];
-	filter.makeElement();
+	// ajax로 데이터 가져오기
+	filter.ajax();
+	
 }
 
+// 검색 요소에 따라 데이터 검색하기
+filter.ajax = function() {
+	alert("데이터 가져오기!");
+	$.ajax({
+		url : "searchList.sb",
+		method : "post",
+		data : {
+			"menu" : filter.menu,
+			"local" : filter.local,
+			"filter" : filter.filter
+		},
+		dataType : "json",
+		success : function(data) {
+			alert("성공!");
+			filter.allItems = data;
+
+			// 검색 조건 contentID별 정렬
+			filter.allItems.sort(function(a,b) {
+				return a.contentid - b.contentid;
+			})
+			
+			// 검색 요소로 검색된 items 다시 그리기
+			filter.makeElement();
+		},
+		error : function(err) {
+			alert("실패!");
+		}
+	});
+
+}
+
+
+// 검색 요소를 저장하는 배열에 추가한 검색 요소 추가
+filter.searchAdd = function(eleName, check_val) {
+	switch(eleName) {
+	case "filter_type":
+		filter.filter.push(check_val);
+		break;
+	case "local_type":
+		filter.local.push(check_val);
+		break;
+	}
+}
+
+// 검색 요소를 저장하는 배열에서 제거한 검색 요소 제거
+filter.searchRemove = function(eleName, check_val) {
+	switch(eleName) {
+	case "filter_type":
+		let idx = filter.filter.findIndex(a => a === check_val)
+		if(idx !== -1) filter.filter.splice(idx, 1);
+		break;
+	case "local_type":
+		let idy = filter.local.findIndex(a => a == check_val)
+		if(idy !== -1) filter.local.splice(idx, 1);
+		break;
+	}
+}
+
+
+// 검색 조건에 추가된 요소 만들기
 filter.makeElement = function() {
 	let widthsort_list_div = $('#widthsort_list');
 	let cardsort_list_div = $('#cardsort_list');
-	
-	// filter.allItems가 비어있으면 모든 요소를 불러와 채워주기
 	
 	// 이전에 검색된 요소들 지우기
 	widthsort_list_div.empty();
