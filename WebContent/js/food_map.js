c558b3a3796a41d7268a8a437ac7f2d9eca84aec
수연 <cjg03194@gmail.com>
2020-05-28 16:59:10 +0900

    20200528 {feat} map에 마커 찍기 기능 추가

--- WebContent/js/food_map.js
+++ WebContent/js/food_map.js
@@ -1,12 +1,142 @@
-$(document).ready(function(){
+var MARKER_WIDTH = 24, // 기본, 클릭 마커의 너비
+MARKER_HEIGHT = 35, // 기본, 클릭 마커의 높이
+OVER_MARKER_WIDTH = 31, // 오버 마커의 너비
+OVER_MARKER_HEIGHT = 41; // 오버 마커의 높이
 
-$("#mapModal").on('shown.bs.modal', function() {
-	var container = document.getElementById('food_map'); //지도를 담을 영역의 DOM 레퍼런스		
-	var options = { //지도를 생성할 때 필요한 기본 옵션
-			center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+var markerSize = new kakao.maps.Size(MARKER_WIDTH, MARKER_HEIGHT), // 기본, 클릭 마커의 크기
+overMarkerSize = new kakao.maps.Size(OVER_MARKER_WIDTH, OVER_MARKER_HEIGHT); // 오버 마커의 크기
+
+//마커 이미지의 이미지 주소입니다
+var normalImageSrc = "http://t1.daumcdn.net/mapjsapi/images/marker.png";
+var starImageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+
+//마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다 
+var positions = [
+	{
+		content: '마커1',//makeContent(i['title'], i['mapx'], i['mapy']),
+		latlng: new kakao.maps.LatLng(33.4581111174, 126.9415156012)
+	}
+];
+
+selectedMarker = null; // 클릭한 마커를 담을 변수
+openinfowindow = null; // 이전에 열린 infowindow를 저장하는 변수
+
+var map; // 지도 객체 저장하는 변수
+
+$("#mapModal").on('shown.bs.modal', function() { // modal 이벤트 발생 시 지도 그리기
+	var mapContainer = document.getElementById('food_map'); //지도를 담을 영역의 DOM 레퍼런스
+
+	var mapOption = { //지도를 생성할 때 필요한 기본 옵션
+			center: new kakao.maps.LatLng(33.4581111174, 126.9415156012), //지도의 중심좌표.
 			level: 3 //지도의 레벨(확대, 축소 정도)
-	};		
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-})
-
+	};
+	
+	map = new kakao.maps.Map(mapContainer, mapOption); //지도 생성 및 객체 리턴
+	
+	//지도 위에 마커를 표시합니다
+	for (var i = 0; i < positions.length; i++) {
+	 	// 마커에 표시할 인포윈도우를 생성합니다 
+	    var infowindow = new kakao.maps.InfoWindow({
+	        content: positions[i].content // 인포윈도우에 표시할 내용
+	    });
+		
+		// 마커를 생성하고 지도위에 표시합니다
+		addMarker(positions[i].latlng, infowindow);
+	}
 });
+
+//마커를 생성하고 지도 위에 표시하고, 마커에 mouseover, mouseout, click 이벤트를 등록하는 함수입니다
+function addMarker(position, infowindow) {
+	console.log(position);
+	console.log(infowindow);
+	// 기본 마커이미지, 오버 마커이미지, 클릭 마커이미지를 생성합니다
+	var normalImage = new kakao.maps.MarkerImage(normalImageSrc, markerSize),
+    overImage = new kakao.maps.MarkerImage(normalImageSrc, overMarkerSize),
+    clickImage = new kakao.maps.MarkerImage(starImageSrc, markerSize);
+	
+	// 마커를 생성하고 이미지는 기본 마커 이미지를 사용합니다
+	var marker = new kakao.maps.Marker({
+	    map: map,
+	    position: position,
+	    image: normalImage 
+	});
+	
+	console.log(marker);
+	
+	// 마커 객체에 마커아이디와 마커의 기본 이미지를 추가합니다
+	marker.normalImage = normalImage;
+	
+	// 마커에 mouseover 이벤트를 등록합니다
+	kakao.maps.event.addListener(marker, 'mouseover', function() {
+		
+	    // 클릭된 마커가 없고, mouseover된 마커가 클릭된 마커가 아니면
+	    // 마커의 이미지를 오버 이미지로 변경합니다
+	    if (!selectedMarker || selectedMarker !== marker) {
+	        marker.setImage(overImage);
+	    }
+	});
+	
+	// 마커에 mouseout 이벤트를 등록합니다
+	kakao.maps.event.addListener(marker, 'mouseout', function() {
+	
+	    // 클릭된 마커가 없고, mouseout된 마커가 클릭된 마커가 아니면
+	    // 마커의 이미지를 기본 이미지로 변경합니다
+	    if (!selectedMarker || selectedMarker !== marker) {
+	        marker.setImage(normalImage);
+	    }
+	});
+	
+	// 마커에 click 이벤트를 등록합니다
+	kakao.maps.event.addListener(marker, 'click', function() {
+		
+	    // 클릭된 마커가 없고, click 마커가 클릭된 마커가 아니면
+	    // 마커의 이미지를 클릭 이미지로 변경합니다
+	    if (!selectedMarker || selectedMarker !== marker) {
+
+			// 이전에 열린 infowindow가 있으면 닫기
+			if(openinfowindow) {
+				openinfowindow.close();
+			}
+			
+	        // 클릭된 마커 객체가 null이 아니면
+	        // 클릭된 마커의 이미지를 기본 이미지로 변경하고
+	        !!selectedMarker && selectedMarker.setImage(selectedMarker.normalImage);
+			
+	        // 현재 클릭된 마커의 이미지는 클릭 이미지로 변경합니다
+	        marker.setImage(clickImage);
+	        
+	        // 인포 윈도우 열기
+	        infowindow.open(map, marker);
+	    }
+	    
+	    // 클릭된 마커를 현재 클릭된 마커 객체로 설정합니다
+	    selectedMarker = marker;
+	    
+	    // 현재 열린 인포 윈도우를 저장
+	    openinfowindow = infowindow;
+	});
+}
+
+
+
+// content를 만들어 반환하는 함수
+function makeContent(title, mapx, mapy) {
+	let makeContent = '<div style="padding:5px;">' + title + ' <br>'
+				+ '<a href="https://map.kakao.com/link/map/' + title + ',' + mapx + ',' + mapy + '" style="color:blue" target="_blank">큰지도보기</a>'
+				+ '<a href="https://map.kakao.com/link/to/' + title + ',' + mapx + ',' + mapy + '" style="color:blue" target="_blank">길찾기</a>';
+				+ '</div>';
+				
+	return makeContent;
+}
+
+// position 배열을 만드는 함수
+function makePosition() {
+	for(let i of filter.allItems) {
+		let pos = {
+			content: '왜 마커 안떠!!!',//makeContent(i['title'], i['mapx'], i['mapy']),
+			latlng: new kakao.maps.LatLng(i['mapy'], i['mapx'])
+		}
+		
+		positions.push(pos);
+	}
+}
