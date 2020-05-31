088e2532e62e849668b44bfc022d9828b4155b6c
수연 <cjg03194@gmail.com>
2020-05-31 17:43:09 +0900

    Auto stash before rebase of "spare"

---
+++ src/service/ApiServiceImpl.java
@@ -0,0 +1,28 @@
+package service;
+
+import java.util.List;
+
+import vo.RestDataVo;
+
+public class ApiServiceImpl implements ApiService {
+
+	@Override
+	public List<RestDataVo> getAreaBasedList(String menu, String local) {
+		
+		return null;
+	}
+
+	@Override
+	public List<RestDataVo> getDetailCommon(String menu) {
+		
+		return null;
+	}
+
+	@Override
+	public StringBuilder getFilterList(String menu, List<String> local, List<String> filter, String findStr,
+			String pageNum) {
+		
+		return null;
+	}
+
+}
